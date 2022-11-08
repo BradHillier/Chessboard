b23432913f7a3d50ee2dde17454495f403ed8266
BradHillier <bradhillier@stumail.viu.ca>
2022-11-08 08:22:36 -0800

    update comment formatting

--- include/piece.h
+++ include/piece.h
@@ -11,18 +11,20 @@ using std::vector;
 // there may be a better way of handling this
 class Chessboard;
 
- /// @brief an abstract chess piece used to implement the various pieces on a chessboard
+/** @brief an abstract chess piece used as the basis to implement 
+*   the various pieces used in a game of chess
+*/
 class Piece 
 {
     protected:
 
-        /// referenceChessboard the piece exists on 
+        /** reference to the Chessboard the piece exists on */
         Chessboard* board_;
 
-        /// a number value used to indicate the pieces type and colour 
+        /** an integer value used to indicate the piece'ss type and colour */
         PieceNum piece_num_;
 
-        /// the pieces current position on the chessboard it exists on
+        /** the pieces current position on the chessboard it exists on */
         Position position_;
 
     public:
@@ -30,30 +32,44 @@ class Piece
         Piece(Chessboard* board, Position position);
         ~Piece() {};
 
-        /// @brief get the pieces associated integer value
-        /// @details Acts as a getter method for the `piece_num_` data member
-        /// @return an integer representing the piece's type and colour. 
+        /** brief get the pieces associated integer value
+        *
+        *   Acts as a getter method for the `piece_num_` data member
+        *   protecting the attribute from accidently being changed
+        *
+        *   @return an integer representing the piece's type and colour. 
+        */
         PieceNum piece_num();
 
-        /// @brief get the pieces current position on the board
-        /// @details a getter method for the `position_` data member
-        /// @return 
+        /** @brief get the pieces current position on the board
+        *
+        *   a getter method for the `position_` data member
+        *
+        *   @return Position containing the piece's row and column
+        */
         Position position();
 
-        /// @brief move the piece to the provided position
-        /// @details  move the piece from it's current position on the 
-        ///           chessboard to the one provided. Acts as a setter method 
-        ///           for the `position_` data member
-        /// @param position on the chessboard to move the piece to 
-        /// @return bool operation success
+        /** @brief move the piece to the provided position
+        *
+        *   move the piece from it's current position on the 
+        *   chessboard to the one provided. Acts as a setter method 
+        *   for the `position_` data member
+        *
+        *   @param position on the chessboard to move the piece to 
+        *   @return bool operation success
+        */
         bool set_position(Position position);
 
-        /// @brief the colour of the piece
-        /// @return bool representing the piece's colour (true if white, false if black)
+        /** @brief the colour of the piece
+        *
+        *   @return true if white, false if black
+        */
         bool Colour();
 
-        /// @brief get all legal moves for the piece
-        /// @return a vector containing all positions on the chessboard the piece can be moved to
+        /** @brief get all legal moves for the piece
+        *
+        *   @return all legal moves from the pieces current position
+        */
         virtual vector<Position> AvailableMoves() = 0;
 };
 
