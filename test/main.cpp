706c911883189d7d46ca1a212510d158a0f2eebf
BradHillier <bradhillier@stumail.viu.ca>
2022-11-10 14:57:02 -0800

    add test program

---
+++ test/main.cpp
@@ -0,0 +1,55 @@
+#include "../include/chessboard.h"
+#include "../include/piece.h"
+#include <iostream>
+using namespace std;
+
+
+char get_cmd()
+{
+   char c;
+   cout << "enter either 'M', 'S' or 'Q': ";
+   cin >> c;
+   return toupper(c);
+}
+
+int main()
+{
+   Chessboard board;
+
+   int c;
+   int x, y;
+   do {
+      
+      Piece* selected = board.selected();
+      if (selected != NULL) { 
+         cout << endl << "currently selected: Position(";
+         cout << selected->position().col << ", "; 
+         cout << selected->position().row << ")";
+      } else {
+         cout << endl << "no piece selected";
+      }
+      board.Print();
+      c= get_cmd();
+
+      switch (c) {
+         case 'M':
+            cout << "enter the position: ";
+            cin >> x >> y;
+            cout << "You entered " << x << ", " << y << endl;;
+            if (board.Move(Position(x, y))) {
+               cout << "successfully move the piece" << endl;
+            } else {
+               cout << "no piece to move" << endl;
+            }
+            break;
+         case 'S':
+            cout << "enter the position: ";
+            cin >> x >> y;
+            cout << "You entered " << x << ", " << y << endl;;
+            board.Select(Position(x, y));
+            break;
+      }
+   } while (c != 'Q');
+}
+
+
