73c3c29519c70718ed46857be78cc900b2bc5758
BradHillier <bradhillier@stumail.viu.ca>
2022-11-10 14:51:43 -0800

    add pawn skeleton

---
+++ src/pawn.cpp
@@ -0,0 +1,15 @@
+#include "../include/pawn.h"
+#include "../include/chessboard.h"
+
+
+Pawn::Pawn(Chessboard* board, Position position, bool colour)
+   : Piece(board, position)
+{
+   piece_num_ = colour ? kWPawn : kBPawn;
+}
+
+
+list<Position> Pawn::AvailableMoves()
+{
+}
+
