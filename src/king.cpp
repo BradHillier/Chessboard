0c1a588b6bf732516c88fd2b8fdbe22b9578503c
Joshua Patrick John Wall <walljp@stumail.csci.viu.ca>
2022-11-16 16:12:38 -0800

    pieces .cpp and .h skeletons

---
+++ src/king.cpp
@@ -0,0 +1,14 @@
+#include "../include/king.h"
+#include "../include/chessboard.h"
+
+
+King::King(Chessboard* board, Position position, bool colour)
+   : Piece(board, position)
+{
+   piece_num_ = colour ? kWKing : kBKing;
+}
+
+
+list<Position> King::AvailableMoves()
+{
+}
