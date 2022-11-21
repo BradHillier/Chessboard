b5cc47f695189b4b24aac46b71d1e1c4a66741c2
BradHillier <bradhillier@stumail.viu.ca>
2022-11-20 20:37:22 -0800

    rename executable files; update makefile

--- test/main.cpp
+++ test/main.cpp
@@ -1,54 +1,19 @@
-#include "../include/chessboard.h"
-#include "../include/piece.h"
-#include <iostream>
-using namespace std;
-
-
-char get_cmd()
-{
-   char c;
-   cout << "enter either 'M', 'S' or 'Q': ";
-   cin >> c;
-   return toupper(c);
-}
+// main.cpp used for testing state machine to be demoed on 19/11                
+                                                                                
+#include "../include/main_menu.h"                                                          
+#include "../include/help.h"                                                               
+#include "../include/playing.h"                                                            
+#include "../include/credits.h"                                                            
+#include "../include/sdl_chess_game.h"
+                                                                                
+#include <iostream>                                                             
+using namespace std;                                                            
+                                                                                
 
 int main()
-{
-   Chessboard board;
-
-   int c;
-   int x, y;
-   do {
-      
-      if (board.selected() != kOffTheBoard) { 
-         cout << endl << "currently selected: Position(";
-         cout << board.selected().col << ", "; 
-         cout << board.selected().row << ")";
-      } else {
-         cout << endl << "no piece selected";
-      }
-      board.Print();
-      c= get_cmd();
-
-      switch (c) {
-         case 'M':
-            cout << "enter the position: ";
-            cin >> x >> y;
-            cout << "You entered " << x << ", " << y << endl;;
-            if (board.Move(Position(x, y))) {
-               cout << "successfully move the piece" << endl;
-            } else {
-               cout << "no piece to move" << endl;
-            }
-            break;
-         case 'S':
-            cout << "enter the position: ";
-            cin >> x >> y;
-            cout << "You entered " << x << ", " << y << endl;;
-            board.Select(Position(x, y));
-            break;
-      }
-   } while (c != 'Q');
-}
-
-
+{                                                                     
+   SDLChessGame* game = new SDLChessGame();
+   game->RunLoop();
+   delete game;
+   return 0;                                                                   
+}  
