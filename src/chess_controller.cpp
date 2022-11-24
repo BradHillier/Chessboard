bda0a73b1e15af15c9b4d627044263549250a8ae
Ricardo Fernandes <658677687@viu.ca>
2022-11-24 23:25:56 +0000

    Implemented ProcessClick() method.

--- src/chess_controller.cpp
+++ src/chess_controller.cpp
@@ -2,18 +2,31 @@
 
 Controller::Controller()
 {
+    game_ = new ChessBoard;
 }
 
 Controller::~Controller()
 {
+    delete game_;       
 }
 
 void Controller::ProcessClick(Position position)
 {
+    // Assumes that ChessBoard's selected() getter method 
+    // returns NULL if no piece is currently selected
+    if (ChessBoard.selected() == NULL)
+    {
+        Chessboard.Select(position);
+    }
+    else
+    {
+        ChessBoard.Move(position);
+    }
 }
 
 unordered_set<Position> GetLegalMoves()
 {
+    
 }
 
 Position GetSelectedPiece()
