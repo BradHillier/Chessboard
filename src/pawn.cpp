2671bd042de989f958b9a5424886eb7556cec388
Joshua Patrick John Wall <walljp@stumail.csci.viu.ca>
2022-11-27 20:20:35 -0800

    Pawn update

--- src/pawn.cpp
+++ src/pawn.cpp
@@ -12,35 +12,38 @@ Pawn::Pawn(Chessboard* board, Position position, bool colour)
 unordered_set<Position> Pawn::LegalMoves()
 {
     unordered_set<Position> moves;
+
     if(Colour() == kBlack){
         if(IsEnemy(position() + Position(1,1))){
             moves.insert(position()+Position(1,1));
         }else if(IsEnemy(position() + Position(-1,1))){
             moves.insert(position() + Position(-1,1));
         }
-        if(IsLegalMove(position()+Position(0,1)) != false){ 
-            if(position() == starting_position()){
-                moves.insert(position() + Position(0,1));
+        if(IsLegalMove((position() + Position(0,1)))){
+            moves.insert(position() + Position(0,1));
+        }
+        if(position() == starting_position()){
+            if(IsLegalMove(position()+Position(0,2))){ 
                 moves.insert(position() + Position(0,2));
-            }else{
-                moves.insert(position() + Position(0,1));
             }
         }
-     }else if(Colour() == kWhite){
-        if(IsEnemy(position() + Position(-1,-1))){
+    }
+
+    if(Colour() == kWhite){
+        if(IsEnemy(position() + Position(-1,-1)) && IsLegalMove(position() + Position(-1,-1))){
             moves.insert(position()+Position(-1,-1));
-        }else if(IsEnemy(position() + Position(1,-1))){
+        }else if(IsEnemy(position() + Position(1,-1)) && IsLegalMove(position() + Position(1,-1))){
             moves.insert(position() + Position(1,-1));
         }
-        if(IsLegalMove(position()+Position(0,-1)) != false){ 
-            if(position() == starting_position()){
-                moves.insert(position() + Position(0,-1));
+        if(IsLegalMove((position() + Position(0,-1)))){
+            moves.insert(position() + Position(0,-1));
+        }
+        if(position() == starting_position()){
+            if(IsLegalMove(position()+Position(0,-2))){ 
                 moves.insert(position() + Position(0,-2));
-            }else{
-                moves.insert(position() + Position(0,-1));
             }
-        }        
-     }
+        }
+    }
     
     return moves;
 }
